c744d7e43306a4a417dc82797b586aa436cc6566
phancykemunto <87754658+phancykemunto@users.noreply.github.com>
2021-08-18 00:45:37 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+
+# Load the Multinomial Naive Bayes model and CountVectorizer object from disk
+#filename = 'voting_clf.pkl'
+cls = pickle.load(open("Review.pkl", 'rb'))
+cv = pickle.load(open('countvector.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST': 
+       Review = request.form['Review']
+       data = [Review]
+       vect = cv.transform(data).toarray()
+       my_prediction = cls.predict(vect)
+       return render_template('result.html',prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,69 @@
+import numpy as np ## scientific computation
+import pandas as pd ## loading dataset file
+##import matplotlib.pyplot as plt ## Visulization
+
+#select data source
+data= pd.read_csv('C:/PYDATAFILES/MOVIES.csv', encoding='latin')
+#output the shape of the data container(frame)
+print(data)
+
+print(data.shape)  ### Return the shape of data 
+print(data.ndim)   ### Return the n dimensions of data
+print(data.size)   ### Return the size of data 
+print(data.isna().sum())  ### Returns the sum fo all na values
+print(data.info())  ### Give concise summary of a DataFrame
+print(data.head())  ## top 5 rows of the dataframe
+print(data.tail()) ## bottom 5 rows of the dataframe
+
+#import seaborn as sns
+#sns.countplot('Sentiment',data=data)
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords ## removing all the stop words
+from gensim.utils import lemmatize
+import re  ## To use Regular expression
+import nltk  ## Preprocessing Reviews
+
+
+
+corpus = []
+for i in range(0,3000):   #we have 3000 reviews
+     review = re.sub('[^a-zA-Z]'," ",data["Review"][i])
+     review = review.lower()
+     review = review.split()
+     pe = WordNetLemmatizer()
+     all_stopword = stopwords.words('english')
+     all_stopword.remove('not')
+     
+     review = " ".join(review)
+     corpus.append(review)
+print(corpus)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500) ##1500 columns
+X = cv.fit_transform(corpus).toarray()
+
+y = data["Sentiment"]
+
+import pickle
+# Creating a pickle file for the CountVectorizer
+pickle.dump(cv, open('countvector.pkl', 'wb'))
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=10)
+
+
+#MultinomialNB
+from sklearn.naive_bayes import MultinomialNB,GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+cls = MultinomialNB()
+cls.fit(X_train, y_train)
+
+cls.score(X_test,y_test)
+
+classifier.score(X_test,y_test)
+
+# Creating a pickle file for the Multinomial Naive Bayes model
+#filename = 'voting_clf.pkl'
+pickle.dump(cls, open("Review.pkl", 'wb'))
